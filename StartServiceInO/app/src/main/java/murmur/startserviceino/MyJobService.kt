bce3944f70236d3ea3a5fa72d6a14e77f27368a9
murmurmuk <tim199980@gmail.com>
2019-09-07 13:56:05 +0800

    update gradle

--- StartServiceInO/app/src/main/java/murmur/startserviceino/MyJobService.kt
+++ StartServiceInO/app/src/main/java/murmur/startserviceino/MyJobService.kt
@@ -5,7 +5,7 @@ import android.app.NotificationManager
 import android.app.job.JobParameters
 import android.app.job.JobService
 import android.content.Context
-import android.support.v4.app.NotificationCompat
+import androidx.core.app.NotificationCompat
 import android.util.Log
 import java.lang.Thread.sleep
 import kotlin.concurrent.thread
